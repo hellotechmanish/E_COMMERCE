9d9226dbe668c3c16004b350d9ef911348cf03dd
Manish kumar <mk772542@gmail.com>
2024-12-24 01:35:29 +0530

    Your commit message

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 // this is all of the imorted file...
 const express = require('express');
 const router = express.Router();
-const { authMiddleware, rolecheck } = require("../middlewares/authMiddleware")
+// const { authMiddleware, rolecheck } = require("../middlewares/authMiddleware")
 const { home, getAllProducts, getProductById, signup, login, logout, forgotPassword, resetPassword } = require('../controllers/userCantroller'); // Functions ko correctly import karo
 
 // user Routes define yaisa karta hai 
@@ -23,14 +23,14 @@ router.post('/logout', logout);
 router.post('/forgot-Password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 // Test Route
-router.get('/admin', authMiddleware, rolecheck(['admin']), (req, res) => {
-    // res.send('Welcome to admin');
-    res.status(200).json({ status: 200, message: 'Welcome to admint' });
-});
+// router.get('/admin', authMiddleware, rolecheck(['admin']), (req, res) => {
+//     // res.send('Welcome to admin');
+//     res.status(200).json({ status: 200, message: 'Welcome to admint' });
+// });
 
-router.get('/user', authMiddleware, rolecheck(['user']), (req, res) => {
-    // res.send('Welcome to user');
-    res.status(200).json({ status: 200, message: 'Welcome to user  ' });
-});
+// router.get('/user', authMiddleware, rolecheck(['user']), (req, res) => {
+//     // res.send('Welcome to user');
+//     res.status(200).json({ status: 200, message: 'Welcome to user  ' });
+// });
 
 module.exports = router;
